88b42db3377fbcb7d30606c3306c05bcbe8ca2d5
AyamotoKohei <koh14taka2@gmail.com>
2021-10-31 22:26:02 +0900

    add: トップ画面に自分が作った予定一覧を作成

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const Schedule = require('../models/schedule');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
+router.get('/', function (req, res, next) {
+  const title = '予定調整くん';
+  // 処理全体を認証済みかで振り分ける
+  if (req.user) {
+    // 条件が合うデータモデルに対応するレコードを全て取得する
+    Schedule.findAll({
+      where: {
+        createdBy: req.user.id // 作成者のユーザーID
+      },
+      order: [['updatedAt', 'DESC']]
+    }).then(schedules => {
+      res.render('index', {
+        title: title,
+        user: req.user,
+        schedules: schedules
+      });
+    });
+  } else {
+    res.render('index', { title: title, user: req.user });
+  }
 });
 
 module.exports = router;
