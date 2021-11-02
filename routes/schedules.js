e75483e76fcfc7a4ec6b031fc6206c2f62bf7741
AyamotoKohei <kohei.cac@gmail.com>
2021-11-02 23:13:31 +0900

    add: 出欠レコードの表示を追加

--- routes/schedules.js
+++ routes/schedules.js
@@ -77,19 +77,45 @@ router.get('/:scheduleId', authenticationEnsurer, (req, res, next) => {
                 }).then((availabilies) => {
                     // 出欠 MapMap(キー:ユーザー ID, 値:出欠Map(キー:候補 ID, 値:出欠))を作成
                     const availabilityMapMap = new Map(); // key: userId, value: Map(key: candidateId, availability)
-                    const map = availabilityMapMap.get(a.user.userId) || new Map();
-                    map.set(a.candidateId, a.availability);
-                    availabilityMapMap.set(a.user.userId, map);
+                    availabilies.forEach((a) => {
+                        const map = availabilityMapMap.get(a.user.userId) || new Map();
+                        map.set(a.candidateId, a.availability);
+                        availabilityMapMap.set(a.user.userId, map);
+                    })
                 });
 
-                console.log(availabilityMapMap);
+                // 閲覧ユーザーと出欠に紐づくユーザーからユーザー Map（キー:ユーザーID, 値:ユーザー）を作る 
+                const userMap = new Map(); // key: userId, value: User
+                userMap.set(parseInt(req.userId), {
+                    isSelf: true,
+                    userId: parseInt(req.userId),
+                    username: req.user.username
+                });
+                availabilies.forEach((a) => {
+                    userMap.set(a.user.userId, {
+                        isSelf: parseInt(req.user.id) === a.user.userId, // 閲覧ユーザー自身であるかを含める
+                        userId: a.user.userId,
+                        username: a.user.username
+                    });
+                });
+
+                // 全ユーザー、全候補で二重ループしてそれぞれの出欠の値がない場合には、「欠席」を設定する
+                const users = Array.from(userMap).map((keyValue) => keyValue[1]);
+                users.forEach((u) => {
+                    candidates.forEach((c) => {
+                        const map =availabilityMapMap.get(u.userId) || new Map();
+                        const a = map.get(c.candidateId) || 0; // デフォルト値は0を利用
+                        map.set(c.candidateId, a);
+                        availabilityMapMap.set(u.userId, map);
+                    });
+                });
 
                 // テンプレートに必要な変数を設定して、テンプレートを描画
                 res.render('schedule', {
                     user: req.user,
                     schedule: schedule,
                     candidates: candidates,
-                    users: [req.user],
+                    users: users,
                     availabilityMapMap: availabilityMapMap
                 });
             });
