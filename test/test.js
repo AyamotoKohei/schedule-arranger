3540dc297296562c84212c718802cdd16d506dae
AyamotoKohei <koh14taka2@gmail.com>
2021-11-03 13:49:07 +0900

    add: 出欠更新のテストの実装

--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ const passportStub = require('passport-stub');
 const app = require('../app');
 const Candidate = require('../models/candidate');
 const Schedule = require('../models/schedule');
+const User = require('../models/user');
+const Availability = require('../models/availability');
 
 // 以下の内容のテストを行う
 // ・レスポンスヘッダの 'Content-Type' が text/html; charset=utf-8 であること
@@ -92,29 +94,84 @@ describe('/schedules', () => {
                         .expect(/テスト候補2/)
                         .expect(/テスト候補3/)
                         .expect(200)
-                        .end((err, res) => {
-                            if (err) return done(err);
-                            // テストで作成したデータを削除
-                            const scheduleId = createdSchedulePath.split('/schedules/')[1];
-                            Candidate.findAll({
-                                where: { scheduleId: scheduleId }
-                            }).then(candidates => {
-                                const promises = candidates.map(c => {
-                                    return c.destroy();
-                                });
-                                // 配列で渡された全ての Promise が終了した際に結果を返す Promise オブジェクトの作成
-                                Promise.all(promises).then(() => {
-                                    // モデルに対応するデータを主キーによって一行だけ取得
-                                    Schedule.findByPk(scheduleId).then(s => {
-                                        s.destroy().then(() => {
-                                            if (err) return done(err);
-                                            done();
-                                        });
-                                    });
-                                });
-                            });
-                        });
+                        .end((err, res) => { deleteScheduleAggregate(createdSchedulePath.split('/schedules/')[1], done, err); });
+
                 });
         });
     });
 });
+
+// 出欠が更新できるかをテストする
+describe('/schedules/:scheduleId/users/:userId/candidayes/:candidateId', () => {
+    // describe 内のテスト前に実行される関数
+    beforeAll(() => {
+        passportStub.install(app);
+        passportStub.login({ id: 0, username: 'testuser' });
+    })
+
+    // describe 内のテスト後に実行される関数
+    afterAll(() => {
+        passportStub.logout();
+        passportStub.uninstall(app);
+    })
+
+    // /schedules に POST を行い予定と候補を作成
+    test('出欠が更新できる', (done) => {
+        User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+            request(app)
+                .post('/schedules')
+                .send({ scheduleName: 'テスト出欠更新予定1', memo: 'テスト出欠更新メモ', candidates: 'テスト出欠更新候補1' })
+                // テストで作成した予定と、そこに紐づく情報を削除するメソッドを呼び出す
+                .end((err, res) => {
+                    const createdSchedulePath = res.headers.location;
+                    const scheduleId = createdSchedulePath.split('/schedules/')[1];
+                    // 予定に関連する候補を取得し、その候補に対してPOSTでWebAPIに対して欠席を出席に更新する
+                    Candidate.findOne({
+                        where: { scheduleId: scheduleId }
+                    }).then((candidates) => {
+                        // 更新されることをテスト
+                        request(app)
+                            .post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidate.candidateId}`)
+                            .send({ availability: 2 }) // 出席に更新
+                            .expect('{"status":"OK", "availability":2}') // 含まれているかどうかをテスト
+                            .end((err, res) => { deleteScheduleAggregate(scheduleId, done, err); });
+                    });
+                });
+        });
+    });
+});
+
+/**
+ * 予定、そこに紐づく出欠・候補を削除する関数
+ * @param {Number} scheduleId スケジュールID
+ * @param {Object} done done
+ * @param {Object} err エラー
+ */
+function deleteScheduleAggregate(scheduleId, done, err) {
+    Availability.findAll({
+        // 全ての出欠情報を取得
+        where: { scheduleId: scheduleId }
+    }).then((availabilities) => {
+        // 全ての出欠情報を削除し、その結果の配列を取得
+        const promises = availabilities.map((a) => { return a.destroy(); });
+        Promise.all(promises).then(() => {
+            Candidate.findAll({
+                // 全ての候補情報を取得
+                where: { scheduleId: scheduleId }
+            }).then((candidates) => {
+                // 全ての候補情報を削除し、その結果の配列を取得
+                const promises = candidates.map((c) => { return c.destroy() });
+                Promise.all(promises).then(() => {
+                    // 全ての予定情報を取得
+                    Schedule.findByPk(scheduleId).then((s) => {
+                        // 全ての予定情報を削除し、その結果の配列を取得
+                        s.destroy().then(() => {
+                            if (err) return done(err);
+                            done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
